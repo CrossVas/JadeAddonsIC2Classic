eb7ded654a44810b294cfd6bf384a908bcc08fd2
CrossVas <cross.wanted.1.4@gmail.com>
2024-02-15 05:29:29 +0900

    reformatted charging/discharging info for BatteryStation and ChargingBench blocks

--- src/main/java/dev/crossvas/jadexic2c/info/BatteryStationInfoProvider.java
+++ src/main/java/dev/crossvas/jadexic2c/info/BatteryStationInfoProvider.java
@@ -5,6 +5,7 @@ import dev.crossvas.jadexic2c.JadeIC2CPluginHandler;
 import dev.crossvas.jadexic2c.utils.ColorMix;
 import dev.crossvas.jadexic2c.utils.Helpers;
 import ic2.api.energy.EnergyNet;
+import ic2.api.items.electric.ElectricItem;
 import ic2.core.block.base.tiles.BaseInventoryTileEntity;
 import ic2.core.block.base.tiles.impls.BaseBatteryStationTileEntity;
 import ic2.core.utils.helpers.Formatters;
@@ -13,6 +14,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.apache.commons.lang3.time.DurationFormatUtils;
@@ -37,26 +39,27 @@ public enum BatteryStationInfoProvider implements IHelper<BlockEntity> {
                 int averageOut = tag.getInt("averageOut");
                 int packetsOut = tag.getInt("packetsOut");
                 if (averageOut > 0) {
-                    Helpers.text(iTooltip, Component.translatable("tooltip.item.ic2.eu_reader.cable_flow_out", Formatters.EU_FORMAT.format((long)averageOut)).withStyle(ChatFormatting.AQUA));
-                    Helpers.text(iTooltip, Component.translatable("tooltip.item.ic2.eu_reader.packet_flow_out", Formatters.EU_FORMAT.format((long)packetsOut)).withStyle(ChatFormatting.AQUA));
+                    Helpers.text(iTooltip, Component.translatable("tooltip.item.ic2.eu_reader.cable_flow_out", Formatters.EU_FORMAT.format(averageOut)).withStyle(ChatFormatting.AQUA));
+                    Helpers.text(iTooltip, Component.translatable("tooltip.item.ic2.eu_reader.packet_flow_out", Formatters.EU_FORMAT.format(packetsOut)).withStyle(ChatFormatting.AQUA));
                 }
 
-                int missingEnergy = tag.getInt("missingEnergy");
+                long missingEnergy = tag.getLong("missingEnergy");
+                int maxTransfer = tag.getInt("maxTransfer");
 
                 if (missingEnergy > 0) {
-                    int slots = 0;
+                    int chargeEnergy = (int) Math.min(maxTransfer, missingEnergy);
+                    Helpers.text(iTooltip, Component.translatable("ic2.probe.chargingBench.eta.name",
+                            DurationFormatUtils.formatDuration(chargeEnergy <= 0 ? 0L : (missingEnergy / chargeEnergy * 50L), "HH:mm:ss")).withStyle(ChatFormatting.GOLD));
+                }
 
-                    int i;
-                    for(i = 0; i < 16; ++i) {
-                        if (!tile.getStackInSlot(i).isEmpty()) {
-                            ++slots;
-                        }
-                    }
+                long maxCapacity = tag.getLong("maxCapacity");
+                long capacity = tag.getLong("capacity");
+                int averageIn = tag.getInt("averageIn");
 
-                    i = Math.min(station.averager.getAverage(), missingEnergy);
-                    Helpers.monoBarLiteral(iTooltip, slots - station.getChargingSlots(), slots, Component.translatable("ic2.probe.discharging.eta.name",
-                            DurationFormatUtils.formatDuration(i <= 0 ? 0L : (missingEnergy / i * 50L), "HH:mm:ss")).withStyle(ChatFormatting.WHITE), ColorMix.BLUE);
-
+                if (capacity > 0) {
+                    int dischargeEnergy = (int) Math.min(averageIn, capacity);
+                    Helpers.barLiteral(iTooltip, (int) capacity, (int) maxCapacity, Component.translatable("ic2.probe.discharging.eta.name",
+                            DurationFormatUtils.formatDuration(dischargeEnergy <= 0 ? 0L : (capacity / dischargeEnergy * 50L), "HH:mm:ss")).withStyle(ChatFormatting.WHITE), ColorMix.BLUE);
                 }
             }
         }
@@ -67,10 +70,27 @@ public enum BatteryStationInfoProvider implements IHelper<BlockEntity> {
         if (blockEntity instanceof BaseInventoryTileEntity tile) {
             if (tile instanceof BaseBatteryStationTileEntity station) {
                 CompoundTag tag = new CompoundTag();
+                long maxCapacity = 0;
+                for (int i = 0; i < station.getChargingSlots(); i++) {
+                    ItemStack battery = station.getStackInSlot(i);
+                    if (!battery.isEmpty()) {
+                        maxCapacity += ElectricItem.MANAGER.getCapacity(battery);
+                    }
+                }
+
+                ItemStack toCharge = station.getStackInSlot(16);
+                long missingEnergy = ElectricItem.MANAGER.getCapacity(toCharge) - ElectricItem.MANAGER.getCharge(toCharge);
+
+                tag.putLong("missingEnergy", missingEnergy);
+                tag.putInt("maxTransfer", ElectricItem.MANAGER.getTransferLimit(toCharge));
+
+                tag.putLong("maxCapacity", maxCapacity);
+                tag.putLong("capacity", station.getMissingEnergy().getIntKey());
+                tag.putInt("averageIn", station.getMissingEnergy().getIntValue());
+
                 CableInfoProvider.EnergyContainer result = CableInfoProvider.getContainer(station);
                 tag.putInt("averageOut", result.getAverageOut());
                 tag.putInt("packetsOut", result.getPacketsOut());
-                tag.putInt("missingEnergy", station.getMissingEnergy().getIntKey());
                 compoundTag.put("BatteryStationInfo", tag);
             }
         }
--- src/main/java/dev/crossvas/jadexic2c/info/ChargingBenchInfoProvider.java
+++ src/main/java/dev/crossvas/jadexic2c/info/ChargingBenchInfoProvider.java
@@ -5,6 +5,7 @@ import dev.crossvas.jadexic2c.JadeIC2CPluginHandler;
 import dev.crossvas.jadexic2c.utils.ColorMix;
 import dev.crossvas.jadexic2c.utils.Helpers;
 import ic2.api.energy.EnergyNet;
+import ic2.api.items.electric.ElectricItem;
 import ic2.core.block.base.tiles.BaseElectricTileEntity;
 import ic2.core.block.base.tiles.impls.BaseChargingBenchTileEntity;
 import ic2.core.utils.helpers.Formatters;
@@ -13,6 +14,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.apache.commons.lang3.time.DurationFormatUtils;
@@ -34,27 +36,28 @@ public enum ChargingBenchInfoProvider implements IHelper<BlockEntity> {
             if (tile instanceof BaseChargingBenchTileEntity bench) {
                 Helpers.text(iTooltip, "ic2.probe.eu.tier.name", EnergyNet.INSTANCE.getDisplayTier(bench.getSinkTier()));
                 Helpers.text(iTooltip, "ic2.probe.eu.max_in.name", bench.getMaxInput());
-
                 int averageIn = tag.getInt("averageIn");
                 int packetsIn = tag.getInt("packetsIn");
                 if (averageIn > 0) {
-                    Helpers.text(iTooltip, Component.translatable("tooltip.item.ic2.eu_reader.cable_flow_in", Formatters.EU_FORMAT.format((long)averageIn)).withStyle(ChatFormatting.AQUA));
-                    Helpers.text(iTooltip, Component.translatable("tooltip.item.ic2.eu_reader.packet_flow_in", Formatters.EU_FORMAT.format((long)packetsIn)).withStyle(ChatFormatting.AQUA));
+                    Helpers.text(iTooltip, Component.translatable("tooltip.item.ic2.eu_reader.cable_flow_in", Formatters.EU_FORMAT.format(averageIn)).withStyle(ChatFormatting.AQUA));
+                    Helpers.text(iTooltip, Component.translatable("tooltip.item.ic2.eu_reader.packet_flow_in", Formatters.EU_FORMAT.format(packetsIn)).withStyle(ChatFormatting.AQUA));
                 }
 
                 int missingEnergy = tag.getInt("missingEnergy");
+                int benchAverageOut = tag.getInt("benchTransferOut");
                 if (missingEnergy > 0) {
-                    int slots = 0;
-                    int i;
-                    for (i = 0; i < 16; ++i) {
-                        if (!bench.getStackInSlot(i).isEmpty()) {
-                            ++slots;
-                        }
-                    }
+                    int i = Math.min(benchAverageOut, missingEnergy);
+                    Helpers.text(iTooltip, Component.translatable("ic2.probe.chargingBench.eta.name",
+                            DurationFormatUtils.formatDuration(i <= 0 ? 0L : (missingEnergy / i * 50L), "HH:mm:ss")).withStyle(ChatFormatting.GOLD));
+                }
 
-                    i = Math.min(bench.averager.getAverage(), missingEnergy);
-                    Helpers.monoBarLiteral(iTooltip, slots - bench.getChargingSlots(), slots, Component.translatable("ic2.probe.chargingBench.eta.name",
-                            DurationFormatUtils.formatDuration(i <= 0 ? 0L : (missingEnergy / i * 50L), "HH:mm:ss")).withStyle(ChatFormatting.WHITE), ColorMix.BLUE);
+                int toDischargeEnergy = tag.getInt("toDischarge");
+                int transferLimit = tag.getInt("transferLimit");
+                int maxCapacity = tag.getInt("maxCapacity");
+                if (toDischargeEnergy > 0) {
+                    int dischargeEnergy =  Math.min(transferLimit, toDischargeEnergy);
+                    Helpers.barLiteral(iTooltip, toDischargeEnergy, maxCapacity, Component.translatable("ic2.probe.discharging.eta.name",
+                            DurationFormatUtils.formatDuration(dischargeEnergy <= 0 ? 0L : (toDischargeEnergy / dischargeEnergy * 50L), "HH:mm:ss")).withStyle(ChatFormatting.WHITE), ColorMix.BLUE);
                 }
             }
         }
@@ -65,7 +68,21 @@ public enum ChargingBenchInfoProvider implements IHelper<BlockEntity> {
         if (blockEntity instanceof BaseElectricTileEntity tile) {
             if (tile instanceof BaseChargingBenchTileEntity bench) {
                 CompoundTag tag = new CompoundTag();
-                tag.putInt("missingEnergy", bench.getMissingEnergy().getIntKey());
+                ItemStack battery = bench.getStackInSlot(16);
+                tag.putInt("toDischarge", ElectricItem.MANAGER.getCharge(battery));
+                tag.putInt("transferLimit", ElectricItem.MANAGER.getTransferLimit(battery));
+                tag.putInt("maxCapacity", ElectricItem.MANAGER.getCapacity(battery));
+
+                int missingEnergy = 0;
+                for (int i = 0; i < 16; i++) {
+                    ItemStack toCharge = bench.getStackInSlot(i);
+                    if (!toCharge.isEmpty()) {
+                        missingEnergy += (ElectricItem.MANAGER.getCapacity(toCharge) - ElectricItem.MANAGER.getCharge(toCharge));
+                    }
+                }
+
+                tag.putInt("missingEnergy", missingEnergy);
+                tag.putInt("benchTransferOut", bench.getMissingEnergy().getIntValue());
                 CableInfoProvider.EnergyContainer result = CableInfoProvider.getContainer(bench);
                 tag.putInt("averageIn", result.getAverageIn());
                 tag.putInt("packetsIn", result.getPacketsIn());
