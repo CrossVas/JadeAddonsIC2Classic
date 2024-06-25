d69312bdc13ee4ca41e80fc275f041657e68c6f6
CrossVas <cross.wanted.1.4@gmail.com>
2024-06-26 07:31:39 +0900

    hopefully fixed FluidTransportStats for pipes

--- src/main/java/dev/crossvas/jadexic2c/info/pipes/BasicPipeInfoProvider.java
+++ src/main/java/dev/crossvas/jadexic2c/info/pipes/BasicPipeInfoProvider.java
@@ -3,16 +3,16 @@ package dev.crossvas.jadexic2c.info.pipes;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import dev.crossvas.jadexic2c.JadeIC2CPluginHandler;
+import dev.crossvas.jadexic2c.elements.SpecialBoxStyle;
 import dev.crossvas.jadexic2c.helpers.IHelper;
-import dev.crossvas.jadexic2c.helpers.PluginHelper;
 import dev.crossvas.jadexic2c.info.removals.TankRender;
 import ic2.core.block.transport.fluid.graph.FluidNet;
 import ic2.core.block.transport.fluid.graph.IFluidPipe;
 import ic2.core.utils.collection.LongAverager;
+import ic2.core.utils.math.ColorUtils;
 import it.unimi.dsi.fastutil.objects.Object2LongMap;
 import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
@@ -23,13 +23,16 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
 import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.ui.IElementHelper;
+import snownee.jade.api.ui.IProgressStyle;
 
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -45,20 +48,22 @@ public class BasicPipeInfoProvider implements IHelper<BlockEntity> {
         }
 
         CompoundTag tag = getData(blockAccessor, "BasicPipeInfo");
+        IElementHelper helper = iTooltip.getElementHelper();
         if (blockAccessor.getBlockEntity() instanceof IFluidPipe) {
             TankRender.TANK_REMOVAL.add(blockAccessor.getBlock());
             ListTag fluidTagList = tag.getList("FluidStacks", Tag.TAG_COMPOUND);
-            List<FluidStack> fluidToDisplay = new ObjectArrayList<>();
             for (Tag fluidTag : fluidTagList) {
                 CompoundTag fluidStackTag = (CompoundTag) fluidTag;
                 Fluid fluid = ForgeRegistries.FLUIDS.getValue(new ResourceLocation(fluidStackTag.getString("fluid")));
                 int avr = fluidStackTag.getInt("average");
                 if (avr > 0 && fluid != null) {
-                    fluidToDisplay.add(new FluidStack(fluid, 1000));
+                    FluidStack fluidStack = new FluidStack(fluid, 1000);
+                    IProgressStyle tankStyle = helper.progressStyle().overlay(helper.fluid(fluidStack));
+                    iTooltip.add(helper.progress(1f, fluidStack.getDisplayName().copy().withStyle(ChatFormatting.WHITE), tankStyle,
+                            new SpecialBoxStyle(fluidStack.getFluid() == Fluids.LAVA ? ColorUtils.doubleDarker(-29925) :
+                                    ColorUtils.doubleDarker(IClientFluidTypeExtensions.of(fluidStack.getFluid()).getTintColor())), false));
                 }
             }
-
-            PluginHelper.gridFluid(iTooltip, "ic2.probe.pipe.transported", ChatFormatting.GOLD, fluidToDisplay);
         }
     }
 
@@ -68,14 +73,14 @@ public class BasicPipeInfoProvider implements IHelper<BlockEntity> {
             CompoundTag tag = new CompoundTag();
             FluidNet.TransportStats stats = FluidNet.INSTANCE.getStats(pipe);
             ListTag fluidTagList = new ListTag();
+            FluidContainer container = FluidContainer.getContainer(pipe);
             for (Fluid fluid : stats.getTransfered().keySet()) {
-                if (fluid != null) {
-                    FluidContainer container = FluidContainer.getContainer(pipe);
-                    CompoundTag fluidStackTag = new CompoundTag();
-                    fluidStackTag.putString("fluid", ForgeRegistries.FLUIDS.getKey(fluid).toString());
-                    fluidStackTag.putInt("average", container.getAverage(fluid));
-                    fluidTagList.add(fluidStackTag);
-                }
+                int avg = container.getAverage(fluid);
+                if (avg <= 0) continue;
+                CompoundTag fluidStackTag = new CompoundTag();
+                fluidStackTag.putString("fluid", ForgeRegistries.FLUIDS.getKey(fluid).toString());
+                fluidStackTag.putInt("average", avg);
+                fluidTagList.add(fluidStackTag);
             }
             if (!fluidTagList.isEmpty()) {
                 tag.put("FluidStacks", fluidTagList);
@@ -127,16 +132,15 @@ public class BasicPipeInfoProvider implements IHelper<BlockEntity> {
             lastTimeIn = currentTime;
             for(Object2LongMap.Entry<Fluid> entry : stats.getTransfered().object2LongEntrySet()) {
                 Fluid fluid = entry.getKey();
-                if (fluid != null) {
-                    long current = entry.getLongValue();
-                    averages.computeIfAbsent(fluid, T -> new LongAverager(20)).addEntry((int)((current - lastFluids.getLong(fluid)) / diff));
-                    lastFluids.put(fluid, current);
-                }
+                long current = entry.getLongValue();
+                averages.computeIfAbsent(fluid, T -> new LongAverager(20)).addEntry((int)((current - lastFluids.getLong(fluid)) / diff));
+                lastFluids.put(fluid, current);
             }
         }
 
         public int getAverage(Fluid fluid) {
-            return (int) this.averages.get(fluid).getAverage();
+            LongAverager avg = this.averages.get(fluid);
+            return avg == null ? 0 : (int) avg.getAverage();
         }
     }
 }
