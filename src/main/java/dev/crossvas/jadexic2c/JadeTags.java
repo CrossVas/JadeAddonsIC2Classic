863fb155bd716f3cbf4330e6197ff442581be809
CrossVas <cross.wanted.1.4@gmail.com>
2024-07-02 18:45:05 +0900

    getting ready for bet release of new version

--- src/main/java/dev/crossvas/jadexic2c/JadeTags.java
+++ src/main/java/dev/crossvas/jadexic2c/JadeTags.java
@@ -4,28 +4,10 @@ import net.minecraft.resources.ResourceLocation;
 
 public class JadeTags {
 
-    public static final String TAG_TEXT = "text";
     public static final String TAG_DATA = "data";
-    public static final String TAG_FLUID = "fluid";
-    public static final String TAG_ITEM = "item";
-    public static final String TAG_ITEM_ID = "itemID";
-    public static final String TAG_ENERGY = "energy";
-    public static final String TAG_MAX = "max";
-    public static final String TAG_INVENTORY = "inventory";
-    public static final String TAG_INVENTORY_FLUID = "inventory_fluid";
-    public static final String TAG_BAR = "bar";
-    public static final String TAG_BAR_COLOR = "bar_color";
-    public static final String TAG_PADDING = "padding";
-    public static final String TAG_PADDING_Y = "padding_y";
 
     public static final ResourceLocation INFO_RENDERER = JadeXIC2C.rl("renderer");
     public static final ResourceLocation WRENCHABLE = JadeXIC2C.rl("wrenchable_info");
-    public static final ResourceLocation EU_READER_INFO = JadeXIC2C.rl("eu_reader");
-    public static final ResourceLocation THERMOMETER_INFO = JadeXIC2C.rl("thermometer");
-    public static final ResourceLocation CROP_INFO = JadeXIC2C.rl("crop");
-    public static final ResourceLocation EU_STORAGE_INFO = JadeXIC2C.rl("eu_storage_info");
-
-    // new tag ids
 
     public static final String JADE_ADDON_TEXT_TAG = "jade_addon_text";
     public static final String JADE_ADDON_ITEM_TAG = "jade_addon_item";
--- src/main/java/dev/crossvas/jadexic2c/helpers/PluginHelper.java
+++ src/main/java/dev/crossvas/jadexic2c/helpers/PluginHelper.java
@@ -1,18 +1,12 @@
 package dev.crossvas.jadexic2c.helpers;
 
-import dev.crossvas.jadexic2c.elements.SpecialItemStackElement;
 import ic2.api.util.DirectionList;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.phys.Vec2;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.fluids.FluidStack;
 import snownee.jade.api.ITooltip;
-import snownee.jade.api.ui.IElement;
-
-import java.util.List;
 
 public class PluginHelper {
 
@@ -39,67 +33,8 @@ public class PluginHelper {
         return fluid.getFluid() == Fluids.LAVA ? -29925 : IClientFluidTypeExtensions.of(fluid.getFluid()).getTintColor() | -16777216;
     }
 
-    public static void spacerX(ITooltip tooltip, int x, boolean append) {
-        if (append) {
-            tooltip.append(tooltip.getElementHelper().spacer(x, 0));
-        } else {
-            tooltip.add(tooltip.getElementHelper().spacer(x, 0));
-        }
-    }
-
     public static void spacerY(ITooltip tooltip, int y) {
         tooltip.add(tooltip.getElementHelper().spacer(0, y));
-    }
-
-    public static void spacerXY(ITooltip tooltip, int x, int y) {
-        tooltip.add(tooltip.getElementHelper().spacer(x, y));
-    }
-
-    public static void item(ITooltip tooltip, ItemStack stack, int x, int y, IElement.Align align, boolean centered) {
-        tooltip.add(new SpecialItemStackElement(stack, align, x, y).centered(centered).size(new Vec2(0, 0)));
-    }
-
-    public static void itemCentered(ITooltip tooltip, ItemStack stack, int y) {
-        tooltip.add(new SpecialItemStackElement(stack, IElement.Align.LEFT, 0, y).centered(true).size(new Vec2(0, 0)));
-    }
-
-    public static void grid(ITooltip iTooltip, String text, ChatFormatting style, List<ItemStack> stackList) {
-        int counter = 0;
-        if (!stackList.isEmpty()) {
-            TextHelper.text(iTooltip, Component.translatable(text).withStyle(style));
-            PluginHelper.spacerY(iTooltip, 2);
-            for (ItemStack stack : stackList) {
-                if (counter < 7) {
-                    iTooltip.append(iTooltip.getElementHelper().item(stack));
-                    counter++;
-                    if (counter == 6) {
-                        counter = 0;
-                        spacerY(iTooltip, 0);
-                    }
-                }
-            }
-            PluginHelper.spacerY(iTooltip, 2);
-        }
-    }
-
-    public static void gridFluid(ITooltip iTooltip, String text, ChatFormatting style, List<FluidStack> stackList) {
-        int counter = 0;
-        if (!stackList.isEmpty()) {
-            TextHelper.text(iTooltip, Component.translatable(text).withStyle(style));
-            PluginHelper.spacerY(iTooltip, 2);
-            for (FluidStack stack : stackList) {
-                if (counter < 7) {
-                    iTooltip.append(iTooltip.getElementHelper().fluid(stack));
-                    spacerX(iTooltip, 3, true);
-                    counter++;
-                    if (counter == 6) {
-                        counter = 0;
-                        spacerY(iTooltip, 0);
-                    }
-                }
-            }
-            PluginHelper.spacerY(iTooltip, 2);
-        }
     }
 
     public static ChatFormatting getColor(int index) {
