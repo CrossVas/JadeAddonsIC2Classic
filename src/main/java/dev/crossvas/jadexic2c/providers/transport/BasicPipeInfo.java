37cede0a20aeed018fab6f368f8352401f1fbe5c
CrossVas <cross.wanted.1.4@gmail.com>
2024-07-22 10:49:34 +0900

    fixed pipe info provider showing fluid stats on non-pipe type blocks

--- src/main/java/dev/crossvas/jadexic2c/providers/transport/BasicPipeInfo.java
+++ src/main/java/dev/crossvas/jadexic2c/providers/transport/BasicPipeInfo.java
@@ -4,10 +4,10 @@ import dev.crossvas.jadexic2c.base.JadeCommonHandler;
 import dev.crossvas.jadexic2c.base.interfaces.IInfoProvider;
 import dev.crossvas.jadexic2c.base.interfaces.IJadeHelper;
 import dev.crossvas.jadexic2c.helpers.FluidContainer;
-import ic2.core.block.machines.tiles.lv.PumpTileEntity;
-import ic2.core.block.machines.tiles.mv.RangedPumpTileEntity;
 import ic2.core.block.transport.fluid.graph.FluidNet;
 import ic2.core.block.transport.fluid.graph.IFluidPipe;
+import ic2.core.block.transport.fluid.tiles.PipeTileEntity;
+import ic2.core.block.transport.item.tubes.FluidTubeTileEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.material.Fluid;
@@ -19,16 +19,23 @@ public class BasicPipeInfo implements IInfoProvider {
 
     @Override
     public void addInfo(IJadeHelper helper, BlockEntity blockEntity, Player player) {
-        if (blockEntity instanceof IFluidPipe fluidPipe && !(blockEntity instanceof PumpTileEntity || blockEntity instanceof RangedPumpTileEntity)) {
-            JadeCommonHandler.TANK_REMOVAL.add(blockEntity);
-            FluidNet.TransportStats stats = FluidNet.INSTANCE.getStats(fluidPipe);
-            FluidContainer container = FluidContainer.getContainer(fluidPipe);
-            for (Fluid fluid : stats.getTransfered().keySet()) {
-                int avg = container.getAverage(fluid);
-                if (avg <= 0) continue;
-                if (fluid != null) {
-                    fluid(helper, new FluidStack(fluid, 1), 1, true);
-                }
+        if (blockEntity instanceof PipeTileEntity pipe) {
+            addPipeInfo(helper, pipe);
+        }
+        if (blockEntity instanceof FluidTubeTileEntity fluidTube) {
+            addPipeInfo(helper, fluidTube);
+        }
+    }
+
+    public void addPipeInfo(IJadeHelper helper, BlockEntity blockEntity) {
+        JadeCommonHandler.TANK_REMOVAL.add(blockEntity);
+        FluidNet.TransportStats stats = FluidNet.INSTANCE.getStats((IFluidPipe) blockEntity);
+        FluidContainer container = FluidContainer.getContainer((IFluidPipe) blockEntity);
+        for (Fluid fluid : stats.getTransfered().keySet()) {
+            int avg = container.getAverage(fluid);
+            if (avg <= 0) continue;
+            if (fluid != null) {
+                fluid(helper, new FluidStack(fluid, 1), 1, true);
             }
         }
     }
